fc572fdf6eb6d3075c46407ed0cfd417d1e5e81a
Krishna Manilal <kris.manilal@gmail.com[B>
2020-05-13 10:41:27 -0700

    New py file as sample 001

---
+++ sample001.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+my_matrix = np.eye(4)
+my_matrix1 = np.array([1,2,3,4],
+			[2,3,4,5],
+			[3,4,5,6],
+			[4,5,6,7])
+print(my_matrix)
+print(my_matrix1)
+
+print('Hello...')
+print('Bye for now!!')
+
